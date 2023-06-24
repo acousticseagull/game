26589be85462e662cdadaa29cd0103fbb342f5a5
acousticseagull <acousticseagull@gmail.com>
2023-06-23 23:25:38 -0400

    1 file added

---
+++ level1.js
@@ -0,0 +1,997 @@
+export default [
+  {
+    tag: 'fighter',
+    x: 412,
+    y: -1902,
+  },
+  {
+    tag: 'carrier',
+    x: 244,
+    y: -8028,
+  },
+  {
+    tag: 'fighter',
+    x: 60,
+    y: -422,
+  },
+  {
+    tag: 'fighter',
+    x: 88,
+    y: -1566,
+  },
+  {
+    tag: 'fighter',
+    x: 208,
+    y: -322,
+  },
+  {
+    tag: 'fighter',
+    x: 224,
+    y: -1902,
+  },
+  {
+    tag: 'fighter',
+    x: 28,
+    y: -4314,
+  },
+  {
+    tag: 'fighter',
+    x: 120,
+    y: -1902,
+  },
+  {
+    tag: 'fighter',
+    x: 232,
+    y: -4414,
+  },
+  {
+    tag: 'fighter',
+    x: 76,
+    y: -4642,
+  },
+  {
+    tag: 'wing',
+    x: 404,
+    y: -2800,
+  },
+  {
+    tag: 'fighter',
+    x: 124,
+    y: -4378,
+  },
+  {
+    tag: 'fighter',
+    x: 324,
+    y: -1422,
+  },
+  {
+    tag: 'fighter',
+    x: 220,
+    y: -574,
+  },
+  {
+    tag: 'fighter',
+    x: 336,
+    y: -646,
+  },
+  {
+    tag: 'wing',
+    x: 168,
+    y: -2700,
+  },
+  {
+    tag: 'fighter',
+    x: 424,
+    y: -1322,
+  },
+  {
+    tag: 'fighter',
+    x: 220,
+    y: -1510,
+  },
+  {
+    tag: 'wing',
+    x: 264,
+    y: -2176,
+  },
+  {
+    tag: 'wing',
+    x: 444,
+    y: -2300,
+  },
+  {
+    tag: 'wing',
+    x: 88,
+    y: -2328,
+  },
+  {
+    tag: 'wing',
+    x: 308,
+    y: -2536,
+  },
+  {
+    tag: 'wing',
+    x: 96,
+    y: -3072,
+  },
+  {
+    tag: 'wing',
+    x: 272,
+    y: -3136,
+  },
+  {
+    tag: 'wing',
+    x: 332,
+    y: -3856,
+  },
+  {
+    tag: 'wing',
+    x: 232,
+    y: -3424,
+  },
+  {
+    tag: 'wing',
+    x: 420,
+    y: -3484,
+  },
+  {
+    tag: 'wing',
+    x: 68,
+    y: -3492,
+  },
+  {
+    tag: 'wing',
+    x: 388,
+    y: -3948,
+  },
+  {
+    tag: 'fighter',
+    x: 300,
+    y: -4590,
+  },
+  {
+    tag: 'fighter',
+    x: 376,
+    y: -4442,
+  },
+  {
+    tag: 'fighter',
+    x: 168,
+    y: -4646,
+  },
+  {
+    tag: 'demon',
+    x: 580,
+    y: -5212,
+  },
+  {
+    tag: 'demon',
+    x: 576,
+    y: -5520,
+  },
+  {
+    tag: 'fighter',
+    x: 144,
+    y: -5846,
+  },
+  {
+    tag: 'imperial',
+    x: 344,
+    y: -6004,
+  },
+  {
+    tag: 'firefly',
+    x: 128,
+    y: -7000,
+  },
+  {
+    tag: 'firefly',
+    x: 340,
+    y: -7000,
+  },
+  {
+    tag: 'fighter',
+    x: 292,
+    y: -5914,
+  },
+  {
+    tag: 'fighter',
+    x: 244,
+    y: -5770,
+  },
+  {
+    tag: 'fighter',
+    x: 192,
+    y: -5914,
+  },
+  {
+    tag: 'carrier',
+    x: 252,
+    y: -6152,
+  },
+  {
+    tag: 'stareater',
+    x: 236,
+    y: -8304,
+  },
+  {
+    tag: 'firefly',
+    x: 104,
+    y: -7956,
+  },
+  {
+    tag: 'fighter',
+    x: 24,
+    y: -7514,
+  },
+  {
+    tag: 'fighter',
+    x: 32,
+    y: -970,
+  },
+  {
+    tag: 'fighter',
+    x: 212,
+    y: -7194,
+  },
+  {
+    tag: 'fighter',
+    x: 384,
+    y: -1066,
+  },
+  {
+    tag: 'fighter',
+    x: 164,
+    y: -970,
+  },
+  {
+    tag: 'fighter',
+    x: 444,
+    y: -970,
+  },
+  {
+    tag: 'imperial',
+    x: 152,
+    y: -6004,
+  },
+  {
+    tag: 'firefly',
+    x: 372,
+    y: -7952,
+  },
+  {
+    tag: 'demon',
+    x: 556,
+    y: -2936,
+  },
+  {
+    tag: 'fighter',
+    x: 276,
+    y: -4126,
+  },
+  {
+    tag: 'fighter',
+    x: 84,
+    y: -2806,
+  },
+  {
+    tag: 'fighter',
+    x: 288,
+    y: -2898,
+  },
+  {
+    tag: 'fighter',
+    x: 344,
+    y: -3330,
+  },
+  {
+    tag: 'fighter',
+    x: 328,
+    y: -3566,
+  },
+  {
+    tag: 'fighter',
+    x: 108,
+    y: -3970,
+  },
+  {
+    tag: 'fighter',
+    x: 188,
+    y: -4054,
+  },
+  {
+    tag: 'fighter',
+    x: 248,
+    y: -5850,
+  },
+  {
+    tag: 'fighter',
+    x: 352,
+    y: -5846,
+  },
+  {
+    tag: 'wing',
+    x: 412,
+    y: -6860,
+  },
+  {
+    tag: 'wing',
+    x: 68,
+    y: -6860,
+  },
+  {
+    tag: 'fighter',
+    x: 264,
+    y: -7294,
+  },
+  {
+    tag: 'fighter',
+    x: 340,
+    y: -7378,
+  },
+  {
+    tag: 'fighter',
+    x: 412,
+    y: -7454,
+  },
+  {
+    tag: 'fighter',
+    x: 128,
+    y: -7374,
+  },
+  {
+    tag: 'fighter',
+    x: 80,
+    y: -7450,
+  },
+  {
+    tag: 'demon',
+    x: 552,
+    y: -8116,
+  },
+  {
+    tag: 'fighter',
+    x: 192,
+    y: -8378,
+  },
+  {
+    tag: 'fighter',
+    x: 112,
+    y: -8242,
+  },
+  {
+    tag: 'fighter',
+    x: 172,
+    y: -8242,
+  },
+  {
+    tag: 'fighter',
+    x: 148,
+    y: -8310,
+  },
+  {
+    tag: 'fighter',
+    x: 296,
+    y: -8242,
+  },
+  {
+    tag: 'fighter',
+    x: 356,
+    y: -8242,
+  },
+  {
+    tag: 'fighter',
+    x: 332,
+    y: -8306,
+  },
+  {
+    tag: 'fighter',
+    x: 284,
+    y: -8378,
+  },
+  {
+    tag: 'imperial',
+    x: 360,
+    y: -9040,
+  },
+  {
+    tag: 'imperial',
+    x: 304,
+    y: -9104,
+  },
+  {
+    tag: 'imperial',
+    x: 232,
+    y: -9148,
+  },
+  {
+    tag: 'imperial',
+    x: 160,
+    y: -9104,
+  },
+  {
+    tag: 'imperial',
+    x: 88,
+    y: -9036,
+  },
+  {
+    tag: 'firefly',
+    x: 248,
+    y: -9600,
+  },
+  {
+    tag: 'stareater',
+    x: 352,
+    y: -10060,
+  },
+  {
+    tag: 'stareater',
+    x: 132,
+    y: -10064,
+  },
+  {
+    tag: 'fighter',
+    x: 244,
+    y: -10098,
+  },
+  {
+    tag: 'fighter',
+    x: 244,
+    y: -9866,
+  },
+  {
+    tag: 'fighter',
+    x: 196,
+    y: -9894,
+  },
+  {
+    tag: 'fighter',
+    x: 292,
+    y: -9898,
+  },
+  {
+    tag: 'fighter',
+    x: 348,
+    y: -9870,
+  },
+  {
+    tag: 'fighter',
+    x: 148,
+    y: -9866,
+  },
+  {
+    tag: 'fighter',
+    x: 84,
+    y: -9914,
+  },
+  {
+    tag: 'fighter',
+    x: 424,
+    y: -9918,
+  },
+  {
+    tag: 'fighter',
+    x: 308,
+    y: -9802,
+  },
+  {
+    tag: 'fighter',
+    x: 204,
+    y: -9802,
+  },
+  {
+    tag: 'fighter',
+    x: 248,
+    y: -10534,
+  },
+  {
+    tag: 'fighter',
+    x: 248,
+    y: -10014,
+  },
+  {
+    tag: 'fighter',
+    x: 248,
+    y: -10202,
+  },
+  {
+    tag: 'fighter',
+    x: 248,
+    y: -10334,
+  },
+  {
+    tag: 'fighter',
+    x: 132,
+    y: -510,
+  },
+  {
+    tag: 'fighter',
+    x: 304,
+    y: -294,
+  },
+  {
+    tag: 'fighter',
+    x: 396,
+    y: -258,
+  },
+  {
+    tag: 'fighter',
+    x: 104,
+    y: -1062,
+  },
+  {
+    tag: 'fighter',
+    x: 308,
+    y: -970,
+  },
+  {
+    tag: 'fighter',
+    x: 324,
+    y: -2326,
+  },
+  {
+    tag: 'fighter',
+    x: 196,
+    y: -2482,
+  },
+  {
+    tag: 'fighter',
+    x: 312,
+    y: -2710,
+  },
+  {
+    tag: 'fighter',
+    x: 184,
+    y: -3682,
+  },
+  {
+    tag: 'imperial',
+    x: 320,
+    y: -4296,
+  },
+  {
+    tag: 'demon',
+    x: 588,
+    y: -6600,
+  },
+  {
+    tag: 'demon',
+    x: 600,
+    y: -10944,
+  },
+  {
+    tag: 'stareater',
+    x: 384,
+    y: -12480,
+  },
+  {
+    tag: 'stareater',
+    x: 56,
+    y: -12484,
+  },
+  {
+    tag: 'stareater',
+    x: 148,
+    y: -12844,
+  },
+  {
+    tag: 'stareater',
+    x: 312,
+    y: -12844,
+  },
+  {
+    tag: 'fighter',
+    x: 52,
+    y: -11998,
+  },
+  {
+    tag: 'fighter',
+    x: 100,
+    y: -12282,
+  },
+  {
+    tag: 'fighter',
+    x: 344,
+    y: -12278,
+  },
+  {
+    tag: 'fighter',
+    x: 44,
+    y: -12338,
+  },
+  {
+    tag: 'fighter',
+    x: 400,
+    y: -12346,
+  },
+  {
+    tag: 'fighter',
+    x: 392,
+    y: -11998,
+  },
+  {
+    tag: 'fighter',
+    x: 220,
+    y: -11998,
+  },
+  {
+    tag: 'fighter',
+    x: 164,
+    y: -11998,
+  },
+  {
+    tag: 'fighter',
+    x: 276,
+    y: -11998,
+  },
+  {
+    tag: 'fighter',
+    x: 332,
+    y: -11998,
+  },
+  {
+    tag: 'fighter',
+    x: 108,
+    y: -11998,
+  },
+  {
+    tag: 'demon',
+    x: 620,
+    y: -13120,
+  },
+  {
+    tag: 'firefly',
+    x: 240,
+    y: -14004,
+  },
+  {
+    tag: 'firefly',
+    x: 300,
+    y: -13972,
+  },
+  {
+    tag: 'firefly',
+    x: 180,
+    y: -13972,
+  },
+  {
+    tag: 'carrier',
+    x: 64,
+    y: -14768,
+  },
+  {
+    tag: 'imperial',
+    x: 256,
+    y: -14964,
+  },
+  {
+    tag: 'imperial',
+    x: 340,
+    y: -15012,
+  },
+  {
+    tag: 'stareater',
+    x: 132,
+    y: -15344,
+  },
+  {
+    tag: 'demon',
+    x: 584,
+    y: -15092,
+  },
+  {
+    tag: 'wing',
+    x: 48,
+    y: -15004,
+  },
+  {
+    tag: 'fighter',
+    x: 420,
+    y: -15626,
+  },
+  {
+    tag: 'fighter',
+    x: 252,
+    y: -15626,
+  },
+  {
+    tag: 'fighter',
+    x: 340,
+    y: -15606,
+  },
+  {
+    tag: 'fighter',
+    x: 380,
+    y: -15606,
+  },
+  {
+    tag: 'fighter',
+    x: 300,
+    y: -15606,
+  },
+  {
+    tag: 'fighter',
+    x: 256,
+    y: -15938,
+  },
+  {
+    tag: 'fighter',
+    x: 216,
+    y: -15882,
+  },
+  {
+    tag: 'imperial',
+    x: 432,
+    y: -16244,
+  },
+  {
+    tag: 'imperial',
+    x: 368,
+    y: -16388,
+  },
+  {
+    tag: 'demon',
+    x: 592,
+    y: -16560,
+  },
+  {
+    tag: 'demon',
+    x: 592,
+    y: -17000,
+  },
+  {
+    tag: 'demon',
+    x: 608,
+    y: -17412,
+  },
+  {
+    tag: 'fighter',
+    x: 36,
+    y: -17226,
+  },
+  {
+    tag: 'fighter',
+    x: 56,
+    y: -17298,
+  },
+  {
+    tag: 'fighter',
+    x: 80,
+    y: -17182,
+  },
+  {
+    tag: 'fighter',
+    x: 96,
+    y: -17262,
+  },
+  {
+    tag: 'fighter',
+    x: 76,
+    y: -17370,
+  },
+  {
+    tag: 'fighter',
+    x: 128,
+    y: -17338,
+  },
+  {
+    tag: 'fighter',
+    x: 168,
+    y: -17398,
+  },
+  {
+    tag: 'fighter',
+    x: 128,
+    y: -17438,
+  },
+  {
+    tag: 'fighter',
+    x: 128,
+    y: -17118,
+  },
+  {
+    tag: 'fighter',
+    x: 84,
+    y: -17078,
+  },
+  {
+    tag: 'fighter',
+    x: 108,
+    y: -17010,
+  },
+  {
+    tag: 'fighter',
+    x: 148,
+    y: -17050,
+  },
+  {
+    tag: 'fighter',
+    x: 160,
+    y: -16954,
+  },
+  {
+    tag: 'fighter',
+    x: 188,
+    y: -16998,
+  },
+  {
+    tag: 'fighter',
+    x: 48,
+    y: -17134,
+  },
+  {
+    tag: 'fighter',
+    x: 388,
+    y: -17186,
+  },
+  {
+    tag: 'wing',
+    x: 480,
+    y: -17720,
+  },
+  {
+    tag: 'wing',
+    x: 312,
+    y: -17716,
+  },
+  {
+    tag: 'stareater',
+    x: 396,
+    y: -17640,
+  },
+  {
+    tag: 'stareater',
+    x: 200,
+    y: -18468,
+  },
+  {
+    tag: 'firefly',
+    x: 364,
+    y: -18364,
+  },
+  {
+    tag: 'firefly',
+    x: 32,
+    y: -18364,
+  },
+  {
+    tag: 'stareater',
+    x: 304,
+    y: -18636,
+  },
+  {
+    tag: 'stareater',
+    x: 96,
+    y: -18636,
+  },
+  {
+    tag: 'fighter',
+    x: 160,
+    y: -18862,
+  },
+  {
+    tag: 'fighter',
+    x: 256,
+    y: -18862,
+  },
+  {
+    tag: 'stareater',
+    x: 208,
+    y: -19052,
+  },
+  {
+    tag: 'imperial',
+    x: 308,
+    y: -19400,
+  },
+  {
+    tag: 'imperial',
+    x: 152,
+    y: -19400,
+  },
+  {
+    tag: 'fighter',
+    x: 240,
+    y: -19534,
+  },
+  {
+    tag: 'fighter',
+    x: 276,
+    y: -19574,
+  },
+  {
+    tag: 'fighter',
+    x: 200,
+    y: -19574,
+  },
+  {
+    tag: 'fighter',
+    x: 100,
+    y: -19606,
+  },
+  {
+    tag: 'fighter',
+    x: 376,
+    y: -19606,
+  },
+  {
+    tag: 'fighter',
+    x: 324,
+    y: -19686,
+  },
+  {
+    tag: 'fighter',
+    x: 160,
+    y: -19686,
+  },
+  {
+    tag: 'fighter',
+    x: 248,
+    y: -19842,
+  },
+  {
+    tag: 'fighter',
+    x: 280,
+    y: -19802,
+  },
+  {
+    tag: 'fighter',
+    x: 216,
+    y: -19802,
+  },
+  {
+    tag: 'fighter',
+    x: 440,
+    y: -19762,
+  },
+  {
+    tag: 'fighter',
+    x: 440,
+    y: -19814,
+  },
+  {
+    tag: 'fighter',
+    x: 440,
+    y: -19862,
+  },
+  {
+    tag: 'fighter',
+    x: 440,
+    y: -19910,
+  },
+  {
+    tag: 'fighter',
+    x: 440,
+    y: -19958,
+  },
+  {
+    tag: 'fighter',
+    x: 440,
+    y: -20006,
+  },
+  {
+    tag: 'blackwidow',
+    x: 244,
+    y: -6748,
+  },
+  {
+    tag: 'blackwidow',
+    x: 208,
+    y: -10816,
+  },
+  {
+    tag: 'blackwidow',
+    x: 380,
+    y: -10816,
+  },
+  {
+    tag: 'mothership',
+    x: 220,
+    y: -20992,
+  },
+  {
+    tag: 'Player',
+    x: 240,
+    y: 360,
+  },
+];
